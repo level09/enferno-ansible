ce9a8ed48add11745c391259a0bffdd5baa04ab2
Nidal <nidal@level09.com>
2014-09-28 19:05:36 +0400

    Ansible script to deploy enferno framework on Ubuntu

---
+++ README.md
@@ -0,0 +1,10 @@
+ansible-simple-git-deploy
+=========================
+
+Example on how to use ansible to deploy simple project with git 
+
+Run this using: 
+
+```
+ansible-playbook -i hosts enferno.yml 
+```
---
+++ roles/enferno/templates/enferno.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+#improve this later
+LOGFILE=/home/{{user_name}}/log/enferno.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=4
+  # user/group to run as
+USER={{user_name}}
+GROUP={{user_name}}
+cd /home/{{user_name}}/{{server_hostname}}
+test -d $LOGDIR || mkdir -p $LOGDIR
+source /home/{{user_name}}/{{server_hostname}}/env/bin/activate
+export ENFERNO_ENV='prod'
+exec gunicorn  --timeout=300 -b 127.0.0.1:5000 manage:app -w $NUM_WORKERS \
+    --user=$USER --group=$GROUP --log-level=debug \
+    --log-file=$LOGFILE 2>>$LOGFILE
+
